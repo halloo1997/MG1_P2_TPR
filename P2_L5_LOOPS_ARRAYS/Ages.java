aa790cc34d070bb0d21ab7d544e8edb48a0b2dc7
werksimon1 <werksimon@gmail.com>
2017-11-28 20:26:12 +0100

    Update Ages.java

--- P2_L5_LOOPS_ARRAYS/Ages.java
+++ P2_L5_LOOPS_ARRAYS/Ages.java
@@ -12,6 +12,7 @@ public class Ages {
         // 2. Maak een array voor de leeftijden van het type int.
 
         // 3. Vraag hoeveel studenten er zijn waarvan je de leeftijd wil weten.
+        //.   Zie onderaan in het commentaar blok hoe je output moet maken.
 
         // 4. Lees het studenten aantal in.
 
@@ -19,11 +20,12 @@ public class Ages {
 
         // 6. Maak nu een for loop van 0 tot het aantal studenten
         //    en stop de ingelezen waarde in de array.
-        //    Wees beleefd en vraag wat de leeftijd is, zie vraag 3. voor de println
+        //    Wees beleefd en vraag elke keer wat de leeftijd is.
 
-        // 7. Maak een variabele aan van het type .. aan waarin het totaal komt te staan
+        // 7. Maak een variabele aan van het type .. aan waarin het totaal komt te staan.
 
-        // 8. Maak een for loop waarin je alle ingevoerde leeftijden op het scherm toont.
+        // 8. Maak een nieuwe for loop waarin je alle ingevoerde leeftijden 
+        //    op het scherm toont. In deze loop tel je ook alle leeftijden bij elkaar op.
 
         // 9. Geef de opgetelde leeftijden weer.
 
@@ -36,9 +38,8 @@ public class Ages {
         Weergave op het scherm
         System.out.println("Hoeveel studenten zijn er.");
 
-        Inlezen van het scherm
-        naam variabele = in.nextInt();
-
+        Een int inlezen van het scherm. 
+        naamVariabele = in.nextInt();
 
         */
 
