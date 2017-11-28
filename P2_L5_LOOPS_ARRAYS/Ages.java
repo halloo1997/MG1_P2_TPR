237d481e26ed50ce2395bba9dd61dd544262c17b
werksimon1 <werksimon@gmail.com>
2017-11-28 15:18:48 +0100

    Add files via upload

---
+++ P2_L5_LOOPS_ARRAYS/Ages.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Ages {
+
+    public static void main(String[] args) {
+
+        // Maak een nieuwe scanner aan voor de invoer
+        Scanner in = new Scanner(System.in);
+
+        // 1. Maak een int voor het aantal studenten.
+
+        // 2. Maak een array voor de leeftijden van het type int.
+
+        // 3. Vraag hoeveel studenten er zijn waarvan je de leeftijd wil weten.
+
+        // 4. Lees het studenten aantal in.
+
+        // 5. Initialiseer de array met het aantal dat je ingelezen hebt.
+
+        // 6. Maak nu een for loop van 0 tot het aantal studenten
+        //    en stop de ingelezen waarde in de array.
+        //    Wees beleefd en vraag wat de leeftijd is, zie vraag 3. voor de println
+
+        // 7. Maak een variabele aan van het type .. aan waarin het totaal komt te staan
+
+        // 8. Maak een for loop waarin je alle ingevoerde leeftijden op het scherm toont.
+
+        // 9. Geef de opgetelde leeftijden weer.
+
+        // 10. Toon de gemiddelde leeftijd van alle studenten op het scherm weer.
+
+
+
+
+        /*
+        Weergave op het scherm
+        System.out.println("Hoeveel studenten zijn er.");
+
+        Inlezen van het scherm
+        naam variabele = in.nextInt();
+
+
+        */
+
+
+    }
+}
