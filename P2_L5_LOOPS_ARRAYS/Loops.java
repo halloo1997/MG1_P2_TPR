7993fb96db5053d4e9b3b9cb0108b2e661b576a4
werksimon1 <werksimon@gmail.com>
2017-11-28 20:04:59 +0100

    Add files via upload

---
+++ P2_L5_LOOPS_ARRAYS/Loops.java
@@ -0,0 +1,26 @@
+package loops;
+import java.util.Scanner;
+
+public class Loops {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        String invoer = in.next();
+        while (!invoer.equals("exit")) {
+            System.out.println("WHILE: " + invoer);
+            invoer = in.next();
+        }
+        System.out.println("Bye Bye While loop");
+
+        do {
+            System.out.println("DO WHILE: " + invoer);
+            invoer = in.next();
+        } while (!invoer.equals("exit"));
+        System.out.println("Leaving Do While loop");
+
+
+    }
+
+}
